1a4d27e4e9b9fd3f9f1bc9e76ba3b0aa0a99a531
Zai <72967098+zaaii@users.noreply.github.com>
2023-01-05 12:51:59 +0800

    Update weebs-api-index.js

--- weebs-api-index.js
+++ weebs-api-index.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.use(morgan("dev"));
 
