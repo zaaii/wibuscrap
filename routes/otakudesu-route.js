e9fdad5209860065f2ecc1f11d6f49bb5e2141d2
Zai <2010817310001@mhs.ulm.ac.id>
2023-01-05 14:12:15 +0800

    yay akhirnya fix

--- routes/otakudesu-route.js
+++ routes/otakudesu-route.js
@@ -7,14 +7,14 @@ route.get("/", (req, res) => {
     res.send({
         endpoint: {
             getOngoingAnime: "/ongoing/:page",
-            getCompletedAnime: "/api/otakudesu/completed/:page",
-            getAnimeSearch: "/api/otakudesu/search/:q",
-            getAnimeList: "/api/otakudesu/anime-list",
-            getAnimeDetail: "/api/otakudesu/detail/:endpoint",
-            getAnimeEpisode: "/api/otakudesu/episode/:endpoint",
-            getBatchLink: "/api/otakudesu/batch/:endpoint",
-            getGenreList: "/api/otakudesu/genres",
-            getGenrePage: "/api/otakudesu/genres/:genre/:page"
+            getCompletedAnime: "/completed/:page",
+            getAnimeSearch: "/search/:q",
+            getAnimeList: "/anime-list",
+            getAnimeDetail: "/detail/:endpoint",
+            getAnimeEpisode: "/episode/:endpoint",
+            getBatchLink: "/batch/:endpoint",
+            getGenreList: "/genres",
+            getGenrePage: "/genres/:genre/:page"
         }
     })
 })
@@ -23,20 +23,20 @@ route.get("/", (req, res) => {
 // Get Ongoing Anime -Done-
 router.get("/ongoing/:page", Services.getOngoing)
 // Get Completed Anime -Done-
-router.get("/api/otakudesu/completed/:page", Services.getCompleted)
+router.get("/completed/:page", Services.getCompleted)
 // Get Search Anime -Done-
-router.get("/api/otakudesu/search/:q", Services.getSearch)
+router.get("/search/:q", Services.getSearch)
 // Get Anime List -Done-
-router.get("/api/otakudesu/anime-list", Services.getAnimeList)
+router.get("/anime-list", Services.getAnimeList)
 // Get Anime Detail -Done-  
-route.get("/api/otakudesu/detail/:endpoint", Services.getAnimeDetail)
+route.get("/detail/:endpoint", Services.getAnimeDetail)
 // Get Anime Episode -Done-
-router.get("/api/otakudesu/episode/:endpoint", Services.getAnimeEpisode)
+router.get("/episode/:endpoint", Services.getAnimeEpisode)
 // Get Batch Link -Done-
-router.get("/api/otakudesu/batch/:endpoint", Services.getBatchLink)
+router.get("/batch/:endpoint", Services.getBatchLink)
 // Get Genre List -Done-
-router.get("/api/otakudesu/genres", Services.getGenreList) 
+router.get("/genres", Services.getGenreList) 
 // Get Genre Page -Done-
-router.get("/api/otakudesu/genres/:genre/:page", Services.getGenrePage)
+router.get("/genres/:genre/:page", Services.getGenrePage)
 
 module.exports = route
